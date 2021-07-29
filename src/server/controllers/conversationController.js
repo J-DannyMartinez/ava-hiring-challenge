80dbf4dae5fcb8340e88d5eed33b4a072b78936d
Danny <codesmith@jdanielmartinez.com>
2021-07-28 19:26:32 -0700

    Add skeleton for controllers, fill out getAll method

---
+++ src/server/controllers/conversationController.js
@@ -0,0 +1,26 @@
+const db = require('../models/conversationsModel');
+
+module.exports = {
+  getAll: (req, res, next) => {
+    res.locals.response = { conversations: [], msg: '', ok: true };
+    const queryString = 'SELECT * FROM "public"."conversations"';
+    db.query(queryString)
+      .then((data) => {
+        console.log(data);
+        res.locals.response.conversations = data;
+        return next();
+      });
+  },
+
+  mutate: (req, res, next) => {
+
+  },
+
+  transform: (req, res, next) => {
+
+  },
+
+  delete: (req, res, next) => {
+
+  },
+};
