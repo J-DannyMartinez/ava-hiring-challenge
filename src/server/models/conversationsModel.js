bb799159d2b6519443724085f036e0ef9bdaa8ac
Danny <codesmith@jdanielmartinez.com>
2021-08-07 11:05:34 -0700

    Add dotenv dependancy for DB privacy

--- src/server/models/conversationsModel.js
+++ src/server/models/conversationsModel.js
@@ -1,7 +1,8 @@
 const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: 'postgres://xxagkkbk:z99W-Mb4qRKyv7eMKFs0mMBirrBUqWsV@kashin.db.elephantsql.com/xxagkkbk',
+  connectionString: process.env.DB_URI,
 });
 
 module.exports = {
